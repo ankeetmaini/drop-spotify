17c69a13f5eaf4f14f68c8c572160f1c95bc9ca0
Ankeet Maini <ankeet.maini@gmail.com>
2021-04-05 20:37:44 +0530

    Boilerplate init

---
+++ src/main/java/com/spotify/app/SpotifyAppApplication.java
@@ -0,0 +1,29 @@
+package com.spotify.app;
+
+import io.dropwizard.Application;
+import io.dropwizard.setup.Bootstrap;
+import io.dropwizard.setup.Environment;
+
+public class SpotifyAppApplication extends Application<SpotifyAppConfiguration> {
+
+    public static void main(final String[] args) throws Exception {
+        new SpotifyAppApplication().run(args);
+    }
+
+    @Override
+    public String getName() {
+        return "SpotifyApp";
+    }
+
+    @Override
+    public void initialize(final Bootstrap<SpotifyAppConfiguration> bootstrap) {
+        // TODO: application initialization
+    }
+
+    @Override
+    public void run(final SpotifyAppConfiguration configuration,
+                    final Environment environment) {
+        // TODO: implement application
+    }
+
+}
---
+++ src/main/java/com/spotify/app/SpotifyAppConfiguration.java
@@ -0,0 +1,10 @@
+package com.spotify.app;
+
+import io.dropwizard.Configuration;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import org.hibernate.validator.constraints.*;
+import javax.validation.constraints.*;
+
+public class SpotifyAppConfiguration extends Configuration {
+    // TODO: implement service configuration
+}
