ceb0e78f4fd3fd39b4faa0f2bcb2473e6aba3743
Ankeet Maini <ankeet.maini@gmail.com>
2021-04-06 09:30:22 +0530

    Add mysql via docker-compose

---
+++ src/main/java/com/spotify/app/dao/SongDAO.java
@@ -0,0 +1,17 @@
+package com.spotify.app.dao;
+
+import com.spotify.app.core.Song;
+
+import org.hibernate.SessionFactory;
+
+import io.dropwizard.hibernate.AbstractDAO;
+
+public class SongDAO extends AbstractDAO<Song> {
+  public SongDAO(SessionFactory factory) {
+    super(factory);
+  }
+
+  public Song getRandom() {
+    // return list()
+  }
+}
